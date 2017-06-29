4e34f1699c335987f0d14483feecf7f28944b706
VadimErm <vadim_ermolenko@ukr.net>
2017-06-29 12:59:42 +0300

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=unifant_test',
+    'dsn' => 'mysql:host=localhost;dbname=test',
     'username' => 'root',
-    'password' => '12345',
+    'password' => 'root',
     'charset' => 'utf8',
 ];
